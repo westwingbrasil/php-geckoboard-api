31e7d0bfe6d37c27f4d38c9d4514d218f29328f0
Eughenio Constantino <eughenio@gmail.com>
2016-07-01 18:09:20 -0300

    Ajuste de conta

--- src/CarlosIO/Geckoboard/Widgets/HighchartsChart.php
+++ src/CarlosIO/Geckoboard/Widgets/HighchartsChart.php
@@ -209,7 +209,7 @@ class HighchartsChart extends Widget
                 ],
                 'renderTo' => 'container',
                 'backgroundColor'=> 'transparent',
-                'lineColor' => 'rgba(35,37,38,100)',
+                'lineColor' => 'rgba(35,37,38,101)',
                 'plotShadow'=> false,
                 'defaultSeriesType' => 'line',
                 'plotBackgroundColor' => null,
